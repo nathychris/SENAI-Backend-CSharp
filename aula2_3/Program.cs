a311e62da6e3ec9e57c0989185e4f0ddf8e2570b
FIC <FIC@info.local>
2019-09-16 14:15:08 -0300

    Terceira estrutura adicional

---
+++ aula2_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace aula2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           // deixamos declarada como falsa por padrão;
+           bool entrada = false;
+
+           int idade;
+           string retorno;
+
+           Console.WriteLine("Digite sua idade");
+           idade = int.Parse(Console.ReadLine());
+           
+           //Como ja atribuimos anteriormente, mudamos o 
+           //estado da variavel para verdadeira caso
+           //entre nessa condição
+           if(idade >= 17){
+               entrada = true;
+           }
+
+           if(entrada == true){
+               retorno = "Entrada permitida!!!";
+
+           }else{
+               retorno = "Entrada não permitida :(";
+
+           }
+
+           Console.WriteLine(retorno);
+
+
+
+
+        }
+    }
+}
