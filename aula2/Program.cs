c07f3684339a7ce457a8dec4115f8788d85b432d
FIC <FIC@info.local>
2019-09-16 12:02:27 -0300

    Adicionando primeira estrutura condicional

---
+++ aula2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace aula2 {
+    class Program {
+        static void Main (string[] args) {
+            double nota1, nota2, nota3, media;
+
+            Console.WriteLine ("Digite sua primeira nota");
+            nota1 = double.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("Digite sua segunda nota");
+            nota2 = double.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("Digite sua terceira nota");
+            nota3 = double.Parse (Console.ReadLine ());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            if (media >= 6) {
+                Console.WriteLine ($"Média: {media} Aluno aprovado");
+
+            } else {
+                Console.WriteLine ($"Média: {media} Aluno reprovado");
+            }
+
+
+        }
+    }
+}
