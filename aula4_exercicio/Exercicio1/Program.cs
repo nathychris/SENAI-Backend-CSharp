4784a5b59e76037d5a76c313c007d58a3ff73cab
FIC <FIC@info.local>
2019-09-20 16:00:44 -0300

    Exercicio Csharp

---
+++ aula4_exercicio/Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] posição = new double[10];
+
+            for ( int i = 0; i <=9; i++){
+
+                Console.WriteLine("Digite sua nota");
+                posição[i] = double.Parse(Console.ReadLine());
+            }
+
+             Console.WriteLine(posição[9]*5);
+        }
+    }
+}
---
+++ aula4_exercicio/Exercicio3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           string [] produto = new string[12];
+           string [] cor = new string[12];
+           double [] tamanho = new double [12];
+           double [] preco = new double [12];
+
+           for( int i=0; i<=11; i++){
+
+               Console.WriteLine("Digite o nome do produto");
+               produto[i] = Console.ReadLine();
+
+               Console.WriteLine("Digite a cor");
+               cor[i] = Console.ReadLine();
+
+               Console.WriteLine("Digite o tamanho");
+               tamanho[i] = double.Parse(Console.ReadLine());
+
+               Console.WriteLine("Digite o preço");
+               preco[i] = double.Parse(Console.ReadLine());
+           }
+
+           for(int i=0; i<=11; i++){
+
+               Console.WriteLine($"Nome do produto{produto}");
+               Console.WriteLine($"Cor{cor}");
+               Console.WriteLine($"Tamanho{tamanho}");
+               Console.WriteLine($"Preço{preco}");
+           }
+        }
+    }
+}
